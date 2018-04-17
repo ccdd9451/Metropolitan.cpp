1607b4d243ea392cd74a7ae454e5577b079c0c02
Kaidong Hu <dongforregister@gmail.com>
2018-04-17 18:48:09 -0400

    stage 1

--- Metropolitan.cpp
+++ Metropolitan.cpp
@@ -1,48 +1,66 @@
 #include "json.hpp"
+
+#include <algorithm>
+#include <random>
+#include <cmath>
+#include <cassert>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
 
+#define PI 3.14159265359
 
 using json = nlohmann::json;
 
 
 class Metropolitan {
+    typedef int VertIndex;
+    typedef std::vector<VertIndex> IndexGroup;
     struct Vertex {
         double x, y;
     };
 
     struct IndeciesEdge {
-        int p1, p2;
+        VertIndex p1, p2;
     };
 
-    struct RefP {
+    struct RefPoint {
         double orient; // x<0.5 => horizontal; x>0.5 => verticle;
-        double Ori;
-        double Min;
-        double Max;
+        double ref;
+        double min;
+        double max;
         double extraCoordinate;
     };
 
-    size_t numRefPs;
+    json metropolitan;
+    size_t numRefPoints;
     std::vector<Vertex> vertices;
     std::vector<IndeciesEdge> edges;
-    std::vector<RefP> refPoints;
-    std::vector< std::vector<int> > indGroups;
+    std::vector<RefPoint> refPoints;
+    std::vector<IndexGroup> indGroups;
     std::vector<IndeciesEdge> filledObs;
 
-    void _parseEdges();
-    void _parseRefPoints();
-    void _parseRefGroups();
-
-    public:
-    json met;
-    Metropolitan(std::string filename);
     void parseJSON();
     void resetVertices();
+    void _parseEdges();
+    void _parseRefPointoints();
+    void _parseRefGroups();
+    void _parseFilledObstacles();
+
+    public:
+    std::vector<double> parameters;
+    Metropolitan(std::string filename);
+    void randomParameter();
+    void applyParameter();
+    // Func f(p0.x, p0.y, p1.x, p1.y);
     template<typename Func> void lineApply(Func f);
+    // adapting SteerSuite
+    // Func f(centerX, centerZ, lengthX, theta)
+    template<typename Func> void orientedObstacleApply(Func f);
+    // Func f(xmin, xmax, ymin, ymax);
+    template<typename Func> void obstacleBoxApply(Func f);
 };
 
 
@@ -52,7 +70,7 @@ Metropolitan::Metropolitan(std::string filename){
     if(!file.is_open()) {
         throw std::runtime_error("not a file");
     } else {
-        file >> met;
+        file >> metropolitan;
     }
     parseJSON();
 }
@@ -60,13 +78,14 @@ Metropolitan::Metropolitan(std::string filename){
 void Metropolitan::parseJSON() {
     resetVertices();
     _parseEdges();
-    _parseRefPoints();
+    _parseRefPointoints();
     _parseRefGroups();
+    _parseFilledObstacles();
 }
 
 void Metropolitan::resetVertices() {
     vertices.clear();
-    const json& jVerts = met["vertexes"];
+    const json& jVerts = metropolitan["vertexes"];
     for (auto it=jVerts.begin();
             it < jVerts.end();
             it += 2) {
@@ -75,7 +94,7 @@ void Metropolitan::resetVertices() {
 }
 
 void Metropolitan::_parseEdges() {
-    const json& jEdges = met["edges"];
+    const json& jEdges = metropolitan["edges"];
     for (auto it=jEdges.begin();
             it < jEdges.end();
             it += 2) {
@@ -86,12 +105,12 @@ void Metropolitan::_parseEdges() {
     }
 }
 
-void Metropolitan::_parseRefPoints() {
-    const json& jRefPs = met["refPoints"];
-    for (auto it=jRefPs.begin();
-            it < jRefPs.end();
+void Metropolitan::_parseRefPointoints() {
+    const json& jRefPoints = metropolitan["refPoints"];
+    for (auto it=jRefPoints.begin();
+            it < jRefPoints.end();
             it += 5) {
-        refPoints.push_back(RefP{
+        refPoints.push_back(RefPoint{
                 (double) *it,
                 (double) *(it+1),
                 (double) *(it+2),
@@ -99,20 +118,105 @@ void Metropolitan::_parseRefPoints() {
                 (double) *(it+4)
                 });
     }
+    numRefPoints = refPoints.size();
 }
 
 void Metropolitan::_parseRefGroups() {
-    const json& jRefGs = met["selectedVertices"];
+    const json& jRefGs = metropolitan["selectedVertices"];
     for (auto& group: jRefGs){
         std::vector<int> refG;
         for (auto& vert: group) {
             refG.push_back((int) vert);
         }
+        indGroups.push_back(refG);
     }
+    assert(indGroups.size() == numRefPoints);
+}
+
+void Metropolitan::_parseFilledObstacles() {
+    const json& jFilledObs = metropolitan["filledObs"];
+    for (auto it=jFilledObs.begin();
+            it < jFilledObs.end();
+            it += 2) {
+        filledObs.push_back(IndeciesEdge{
+                (int)*it,
+                (int)*(it+1)
+                });
+    }
+}
+
+void Metropolitan::randomParameter() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> rand(0., 1.);
+
+    for (size_t i=0; i<numRefPoints; i++) {
+        RefPoint& refP = refPoints[i];
+        double param = refP.min + rand(gen) * (refP.max - refP.min);
+        parameters.push_back(param);
+    }
+}
+
+void Metropolitan::applyParameter() {
+    resetVertices();
+    for (size_t i=0; i<numRefPoints; i++) {
+        RefPoint& refP = refPoints[i];
+        double diff = parameters[i] - refP.ref;
+        if (refP.orient < 0.5) {
+            for (auto vIdx: indGroups[i]) {
+                vertices[vIdx].x += diff;
+            }
+        } else {
+            for (auto vIdx: indGroups[i]) {
+                vertices[vIdx].y += diff;
+            }
+        }
+    }
+}
+
+template<typename Func>
+void Metropolitan::lineApply(Func f) {
+    for (auto& e: edges) {
+        const Vertex &v1=vertices[e.p1];
+        const Vertex &v2=vertices[e.p2];
+        f(v1.x, v1.y, v2.x, v2.y);
+    }
+}
+
+template<typename Func>
+void Metropolitan::obstacleBoxApply(Func f) {
+    for (auto& e: edges) {
+        const Vertex &v1=vertices[e.p1];
+        const Vertex &v2=vertices[e.p2];
+        double xmin = std::min(v1.x, v2.x);
+        double xmax = std::max(v1.x, v2.x);
+        double ymin = std::min(v1.y, v2.y);
+        double ymax = std::max(v1.y, v2.y);
+        f(xmin, xmax, ymin, ymax);
+    }
+}
+
+template<typename Func>
+void Metropolitan::orientedObstacleApply(Func f) {
+    auto warp = [f](double x0, double y0, double x1, double y1){
+        double centerX = (x0 + x1)/2;
+        double centerZ = (y0 + y1)/2;
+        double diffX = (x0 - x1);
+        double diffY = (y0 - y1);
+        double lengthX = std::sqrt(diffX*diffX + diffY*diffY);
+        double theta = std::atan2(diffX, diffY)*180/PI;
+        f(centerX, centerZ, lengthX, theta);
+    };
+    lineApply(warp);
 }
 
 int main()
 {
     Metropolitan m("j_metropolitan.json");
+    m.randomParameter();
+    m.applyParameter();
+    m.lineApply([](double a, double b, double c, double d){
+            std::cout << "vert" << a << " " << b << " " << c << " " << d << std::endl;
+            });
     return 0;
 }
